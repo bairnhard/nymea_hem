6d893849f817b050e0e192003850eae5a0b543fd
fischit87 <nils@welzn.de>
2026-07-01 14:36:26 +0200

    Add constants for thing class and state attributes
    Added new attributes for thing class and state management.

--- custom_components/nymea_hem/const.py
+++ custom_components/nymea_hem/const.py
@@ -1,6 +1,11 @@
 """Constants for Nymea HEM integration."""
 
+from homeassistant.const import Platform
+
 DOMAIN = "nymea_hem"
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
 CONF_HOST = "host"
 CONF_PORT = "port"
 CONF_USERNAME = "username"
@@ -16,7 +21,14 @@ JSONRPC_HELLO_METHOD = "JSONRPC.Hello"
 JSONRPC_AUTH_METHOD = "JSONRPC.Authenticate"
 INTEGRATIONS_GET_THINGS = "Integrations.GetThings"
 INTEGRATIONS_GET_THING_STATE = "Integrations.GetThingState"
+INTEGRATIONS_GET_THING_CLASSES = "Integrations.GetThingClasses"
 
 ATTR_DEVICE_ID = "device_id"
 ATTR_DEVICE_NAME = "device_name"
 ATTR_DEVICE_TYPE = "device_type"
+ATTR_THING_CLASS_ID = "thing_class_id"
+ATTR_THING_CLASS_NAME = "thing_class_name"
+ATTR_STATE_TYPE_ID = "state_type_id"
+ATTR_STATE_NAME = "state_name"
+ATTR_VALUE_IN_STATE = "value_in_state"
+ATTR_VALUE_PAYLOAD = "value_payload"
